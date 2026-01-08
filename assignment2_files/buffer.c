cf14fec1ae69832da52558093f543e7f1501cd27
Alies <47625063+Alies03@users.noreply.github.com>
2026-01-08 13:40:26 +0100

    feat: Add some pre and post conditions to put and get

--- assignment2_files/buffer.c
+++ assignment2_files/buffer.c
@@ -4,6 +4,18 @@ int tail = 0;
 int const maxlen = 4;
 
 
+/*@
+	requires \valid(output_arr);
+	requires 0 <= tail < maxlen;
+	requires \valid(buffer);
+	requires \valid(buffer + (0.. maxlen-1));
+	assigns *output_arr;
+	ensures \result == -1 ==> head == tail;
+	ensures \result == 0 ==> (*output_arr == buffer[\old(tail)]);
+	ensures 0 <= tail < maxlen;
+	ensures \old(tail) + 1 == maxlen ==> tail == 0;
+	ensures \old(tail) + 1 != maxlen ==> tail == \old(tail) + 1;
+*/
 int get(int *output_arr) {
 	int next;
 
@@ -20,6 +32,16 @@ int get(int *output_arr) {
 }
 
 
+/*@
+  requires 0 <= head < maxlen;
+  requires \valid(buffer + (0 .. maxlen - 1));
+  assigns head, buffer[\old(head)];
+  ensures 0 <= head < maxlen;
+  ensures buffer[\old(head)] == data;
+  ensures \result == -1 ==> head == tail;
+  ensures \old(head) + 1 == maxlen ==> head == 0;
+  ensures \old(head) + 1 != maxlen ==> head == \old(head) + 1;
+*/
 int put(int data) {
 	int next;
 	next = head + 1;
