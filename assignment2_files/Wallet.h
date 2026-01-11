d616195448bd987805f8ff5d1eb6ab9a652aaebf
Kevin <kevinbraam10@gmail.com>
2026-01-11 20:20:49 +0100

    Added logical specification Wallet

--- assignment2_files/Wallet.h
+++ assignment2_files/Wallet.h
@@ -1,5 +1,12 @@
- //interface Wallet 
- 
- void pay(int price);
- 
- void doublingCharm();
+//interface Wallet 
+//@ ghost int count;
+//@ global invariant I: count >= 0;
+
+/*@
+    requires count>=price;
+    ensures count == \old(count) - price;
+*/
+void pay(int price);
+
+//@ ensures count == \old(count) * 2;
+void doublingCharm();
